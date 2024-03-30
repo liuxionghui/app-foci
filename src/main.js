1081fd3628f89681f0cc20ffc1d5c07132a9e907
zhongyuhua1990happyyear <773637906@qq.com>
2024-03-30 20:12:52 +0800

    fix: 测试github连接

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './route'
-import store from './store/index.js'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './route';
+import store from './store/index.js';
 
 // import '../public/js/iscroll-probe.js'
 
+import FociContent from './common/FociContent.vue';
+Vue.component('foci-content', FociContent);
 
-import FociContent from './common/FociContent.vue'
-Vue.component('foci-content',FociContent)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
